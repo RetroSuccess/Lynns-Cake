a8ee4c2d193dd6327d75307ba7dcc07a76f35c38
RetroSuccess <165993473+RetroSuccess@users.noreply.github.com>
2024-05-30 07:59:55 +0200

    Add files via upload

---
+++ Lynnes Cake/Form1.Designer.vb
@@ -0,0 +1,359 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txtNcake = New System.Windows.Forms.TextBox()
+        Me.txtCamount = New System.Windows.Forms.TextBox()
+        Me.txtCprice = New System.Windows.Forms.TextBox()
+        Me.txtCtotal = New System.Windows.Forms.TextBox()
+        Me.txtNbread = New System.Windows.Forms.TextBox()
+        Me.txtBamount = New System.Windows.Forms.TextBox()
+        Me.txtBprice = New System.Windows.Forms.TextBox()
+        Me.txtBtotal = New System.Windows.Forms.TextBox()
+        Me.txtTamount = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(304, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(176, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "LYNN'S CAKES"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtCtotal)
+        Me.GroupBox1.Controls.Add(Me.txtCprice)
+        Me.GroupBox1.Controls.Add(Me.txtCamount)
+        Me.GroupBox1.Controls.Add(Me.txtNcake)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(47, 80)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(253, 180)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Cakes"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtBtotal)
+        Me.GroupBox2.Controls.Add(Me.txtBprice)
+        Me.GroupBox2.Controls.Add(Me.txtBamount)
+        Me.GroupBox2.Controls.Add(Me.txtNbread)
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(468, 80)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(243, 180)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Bread"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.txtTotal)
+        Me.GroupBox3.Controls.Add(Me.txtTamount)
+        Me.GroupBox3.Controls.Add(Me.Label11)
+        Me.GroupBox3.Controls.Add(Me.Label10)
+        Me.GroupBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(229, 280)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(280, 100)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Grand total"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(148, 401)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 37)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(337, 401)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(87, 37)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(528, 401)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(87, 37)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Name of cake"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 70)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Quantity"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 114)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Price"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(15, 147)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 13)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Total price"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(20, 31)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(90, 13)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Name of bread"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(20, 70)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 13)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Quantity"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(20, 114)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(36, 13)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = "Price"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(20, 147)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(68, 13)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "Total price"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(15, 25)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(135, 13)
+        Me.Label10.TabIndex = 8
+        Me.Label10.Text = "Total number of orders"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(15, 63)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(93, 13)
+        Me.Label11.TabIndex = 9
+        Me.Label11.Text = "Total payments"
+        '
+        'txtNcake
+        '
+        Me.txtNcake.Location = New System.Drawing.Point(132, 28)
+        Me.txtNcake.Name = "txtNcake"
+        Me.txtNcake.Size = New System.Drawing.Size(100, 20)
+        Me.txtNcake.TabIndex = 4
+        '
+        'txtCamount
+        '
+        Me.txtCamount.Location = New System.Drawing.Point(132, 67)
+        Me.txtCamount.Name = "txtCamount"
+        Me.txtCamount.Size = New System.Drawing.Size(100, 20)
+        Me.txtCamount.TabIndex = 5
+        '
+        'txtCprice
+        '
+        Me.txtCprice.Location = New System.Drawing.Point(132, 111)
+        Me.txtCprice.Name = "txtCprice"
+        Me.txtCprice.Size = New System.Drawing.Size(100, 20)
+        Me.txtCprice.TabIndex = 6
+        '
+        'txtCtotal
+        '
+        Me.txtCtotal.Location = New System.Drawing.Point(132, 147)
+        Me.txtCtotal.Name = "txtCtotal"
+        Me.txtCtotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtCtotal.TabIndex = 7
+        '
+        'txtNbread
+        '
+        Me.txtNbread.Location = New System.Drawing.Point(127, 28)
+        Me.txtNbread.Name = "txtNbread"
+        Me.txtNbread.Size = New System.Drawing.Size(100, 20)
+        Me.txtNbread.TabIndex = 8
+        '
+        'txtBamount
+        '
+        Me.txtBamount.Location = New System.Drawing.Point(127, 67)
+        Me.txtBamount.Name = "txtBamount"
+        Me.txtBamount.Size = New System.Drawing.Size(100, 20)
+        Me.txtBamount.TabIndex = 9
+        '
+        'txtBprice
+        '
+        Me.txtBprice.Location = New System.Drawing.Point(127, 107)
+        Me.txtBprice.Name = "txtBprice"
+        Me.txtBprice.Size = New System.Drawing.Size(100, 20)
+        Me.txtBprice.TabIndex = 10
+        '
+        'txtBtotal
+        '
+        Me.txtBtotal.Location = New System.Drawing.Point(127, 144)
+        Me.txtBtotal.Name = "txtBtotal"
+        Me.txtBtotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtBtotal.TabIndex = 11
+        '
+        'txtTamount
+        '
+        Me.txtTamount.Location = New System.Drawing.Point(165, 22)
+        Me.txtTamount.Name = "txtTamount"
+        Me.txtTamount.Size = New System.Drawing.Size(100, 20)
+        Me.txtTamount.TabIndex = 12
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(165, 60)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotal.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtCtotal As TextBox
+    Friend WithEvents txtCprice As TextBox
+    Friend WithEvents txtCamount As TextBox
+    Friend WithEvents txtNcake As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtBtotal As TextBox
+    Friend WithEvents txtBprice As TextBox
+    Friend WithEvents txtBamount As TextBox
+    Friend WithEvents txtNbread As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtTamount As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Lynnes Cake/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cake As String
+        Dim Qcake As Integer
+        Dim Pcake As Integer
+        Dim Tcake As Integer
+
+        Dim bread As String
+        Dim Qbread As Integer
+        Dim Pbread As Integer
+        Dim Tbread As Integer
+
+        Dim Torders As Integer
+        Dim Total As Integer
+
+        cake = txtNcake.Text
+        Qcake = txtCamount.Text
+        Pcake = txtCprice.Text
+        txtCtotal.Text = Qcake * Pcake
+
+        bread = txtBamount.Text
+        Qbread = txtBamount.Text
+        Pbread = txtBprice.Text
+        txtBtotal.Text = Qbread * Pbread
+
+        txtTamount.Text = Qcake + Qbread
+        txtTotal.Text = (Qcake * Pcake) + (Qbread * Pbread)
+
+    End Sub
+End Class
